66350844629043e5f9f7cd987fb439da504371d8
Sorck <juanluis021001@gmail.com>
2023-05-08 20:39:51 -0600

    Se realizan los cambios a el código para que sea funcional de acuerdo a lo que se visualizó, todo acorde a la tarea número dos de revisión de código

--- formulario.js
+++ formulario.js
@@ -1,97 +1,80 @@
-var formulario = document.querySelector("#form")
+//Este es un código de javaScript que se encarga de realizar la inclusión de 
+//una tarjeta con un nombre, edad y nacionalidad de personas para ser invitados
+//a una fiesta y tener la opción de borrar personas de esto mismo.
 
-formulario.onsubmit = function(e) {
+var formulario = document.querySelector(".formulario");
 
-  e.prevent();
-  
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+formulario.onsubmit = function(event) {
+  event.preventDefault();
 
-  var nombre = n.value
-  var edad = e.value
+  var nombreInput = formulario.elements[0]; // Renombrar la variable 'n' a 'nombreInput'
+  var edadInput = formulario.elements[1]; // Renombrar la variable 'e' a 'edadInput'
+  var nacionalidadSelect = formulario.elements[2]; // Renombrar la variable 'na' a 'nacionalidadSelect'
 
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
-  console.log(nombre, edad)
-  console.log(nacionalidad)
+  var nombre = nombreInput.value;
+  var edad = parseInt(edadInput.value);
+  var indiceSeleccionado = nacionalidadSelect.selectedIndex; // Renombrar la variable 'i' a 'indiceSeleccionado'
+  var nacionalidad = nacionalidadSelect.options[indiceSeleccionado].value;
+
+  console.log(nombre, edad);
+  console.log(nacionalidad);
 
   if (nombre.length === 0) {
-    n.classList.add("error")
+    nombreInput.classList.add("error"); // Renombrar la variable 'n' a 'nombreInput'
   }
-  if (edad < 18 || edad > 120) {
-    e.classList.add("error")
+  if (edad < 18 || edad > 120 || isNaN(edad)) {
+    edadInput.classList.add("error"); // Renombrar la variable 'e' a 'edadInput'
   }
 
-if (nombre.length > 0 
-  && (edad > 18 
-    && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
+  if (nombre.length > 0 && edad >= 18 && edad <= 120) {
+    agregarInvitado(nombre, edad, nacionalidad);
   }
 }
-
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-document.body.appendChild(corteLinea)
-document.body.appendChild(botonBorrar);
-
+//if para agregar invitados
 function agregarInvitado(nombre, edad, nacionalidad) {
-
   if (nacionalidad === "ar") {
-    nacionalidad = "Argentina"
+    nacionalidad = "Argentina";
   }
   else if (nacionalidad === "mx") {
-    nacionalidad = "Mexicana"
+    nacionalidad = "Mexicana";
   }
   else if (nacionalidad === "vnzl") {
-    nacionalidad = "Venezolana"
+    nacionalidad = "Venezolana";
   }
   else if (nacionalidad === "per") {
-    nacionalidad = "Peruana"
+    nacionalidad = "Peruana";
   }
 
-var lista = document.getElementById("lista-de-invitados")
+  var listaInvitados = document.getElementById("lista-de-invitados"); // Renombrar la variable 'lista' a 'listaInvitados'
 
-var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
-lista.appendChild(elementoLista)
+  var elementoLista = document.createElement("div");
+  elementoLista.classList.add("elemento-lista");
+  listaInvitados.appendChild(elementoLista);
 
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = "Nombre: "
-inputNombre.value = nombre 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
+  function crearElemento(descripcion, valor) {
+    var spanDescripcion = document.createElement("span"); // Renombrar la variable 'spanNombre' a 'spanDescripcion'
+    var inputValor = document.createElement("input"); // Renombrar la variable 'inputNombre' a 'inputValor'
+    var espacio = document.createElement("br");
+    spanDescripcion.textContent = descripcion + ": ";
+    inputValor.value = valor;
+    elementoLista.appendChild(spanDescripcion);
+    elementoLista.appendChild(inputValor);
+    elementoLista.appendChild(espacio);
+  }
+//Creación de elementos para poder hacer funcional la tarjeta con su botón de borrar
+  crearElemento("Nombre", nombre);
+  crearElemento("Edad", edad);
+  crearElemento("Nacionalidad", nacionalidad);
 
-function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = descripcion + ": "
-inputNombre.value = valor 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
-}
+  var botonBorrar = document.createElement("button");
+  botonBorrar.textContent = "Eliminar invitado";
+  botonBorrar.id = "boton-borrar";
+  var corteLinea = document.createElement("br");
+  elementoLista.appendChild(corteLinea);
+  elementoLista.appendChild(botonBorrar);
 
-crearElemento("Nombre", nombre)
-crearElemento("Edad", edad)
-crearElemento("Nacionalidad", nacionalidad)
-
-
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-elementoLista.appendChild(corteLinea)
-elementoLista.appendChild(botonBorrar);
-
- botonBorrar.onclick = function() {
-// this.parentNode.style.display = 'none';
-botonBorrar.parentNode.remove()
+  botonBorrar.onclick = function() {
+    this.parentNode.style.display = 'none';
+    botonBorrar.parentNode.remove();
   }
 }
